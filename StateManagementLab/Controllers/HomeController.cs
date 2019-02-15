3c9b39efe0051d1fef9274bc652c5ab9819a86a8
Kent Butler <KentDavidButler@gmail.com>
2019-02-14 22:27:28 -0500

    initial commit

---
+++ StateManagementLab/Controllers/HomeController.cs
@@ -0,0 +1,117 @@
+﻿using StateManagementLab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StateManagementLab.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            ViewBag.CurrentUser = (User)Session["CurrentUser"];
+            return View();
+        }
+
+        public ActionResult Register(string Message)
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(User newUser)
+        {
+            if (Session["AllUsers"] != null)
+            {
+                Session["AllUsers"] = new List<User>();
+            }
+            else
+            {
+                List<User> AllUsers = new List<User> { new User("Kent", "Butler", "photokentbutler@gmail.com", 35, "password", "password") };
+            }
+
+            if(string.IsNullOrEmpty(newUser.ConfirmPassword))
+            {// this is for existing user logging in
+                foreach (User user in AllUsers)
+                {// searches list for email address entered and checks if it exisists.
+                    if (user.Email == newUser.Email)
+                    {
+                        if (user.Password == newUser.Password)
+                        {
+                            ViewBag.CurrentUser = user;
+                            Session["CurrentUser"] = user;
+
+                            return View();
+                        }
+                        else
+                        {
+                            ViewBag.ErrorMessage = "Login failed. Try again.";
+                            return RedirectToAction("Login");
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.ErrorMessage = "Login failed. Try again.";
+                        return RedirectToAction("Login");
+                    }
+                }
+            }
+
+
+            if (newUser.Password != newUser.ConfirmPassword)
+            {
+                return RedirectToAction("Register", new { message = "Error: Passwords did not match" });
+            }
+
+            if (Session["CurrentUser"] != null)
+            {
+                
+                newUser = (User)Session["CurrentUser"];
+                ViewBag.CurrentUser = newUser;
+            }
+
+            if (ModelState.IsValid)
+            {
+                ViewBag.CurrentUser = newUser;
+                Session["CurrentUser"] = newUser;
+                AllUsers.Add(newUser);
+                return View();
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Registration failed. Try again.";
+                return RedirectToAction("Register");
+            }
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.CurrentUser = (User)Session["CurrentUser"];
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.CurrentUser = (User)Session["CurrentUser"];
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Remove("CurrentUser");
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ StateManagementLab/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StateManagementLab.Models
+{
+    public class User
+    {
+        [Required]
+        [RegularExpression(@"^[A-Z{1}]+[a-zA-z{1,30}]+$", ErrorMessage = "Please enter a valid name.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z{1}]+[a-zA-z{1,30}]+$", ErrorMessage = "Please enter a valid name.")]
+        public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9{5,30}]+@[a-zA-A0-9{5,10}]+\.[a-zA-Z0-9{2,3}]+$", ErrorMessage = "Incorrect E-mail Format!")]
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string ConfirmPassword { get; set; }
+
+
+        public User(string firstName, string lastName, string email, int age, string password, string confirmPassword)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Age = age;
+            Password = password;
+            ConfirmPassword = confirmPassword;
+        }
+
+
+        public User() { }
+    }
+}
